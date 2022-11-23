2d7f60e2abcf4d73c93fb83760294a36ffd70c5d
Ahmed Salem <85931758+ahmed-mrbeam@users.noreply.github.com>
2022-11-23 15:17:02 +0100

    SW-2270-change-registry-url-to-alpha-url-of-mr-beam-cloud (#51)
    * Changed the find mr beam registry link to the alpha cloud instance
    * Added reference to the revert issue id

--- octoprint_findmymrbeam/__init__.py
+++ octoprint_findmymrbeam/__init__.py
@@ -186,7 +186,10 @@ class FindMyMrBeamPlugin(octoprint.plugin.AssetPlugin,
 
 	def get_settings_defaults(self):
 		return dict(enabled=True,
-					url="https://find.mr-beam.org/registry",
+					# Revert this change before beta release(https://mr-beam.atlassian.net/browse/SW-2272)
+					# in https://mr-beam.atlassian.net/browse/SW-2273
+					# url="https://find.mr-beam.org/registry",
+					url="https://alpha-dot-find-mr-beam.appspot.com/registry",
 					interval_client=300.0,
 					interval_noclient=60.0,
 					# configured in config.yaml in appearance:{name: aBook}
